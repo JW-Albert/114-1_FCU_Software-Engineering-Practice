35618f09b32862463fade7f42d12ee3cb1421e84
JW-Albert <ru04jo30801@gmail.com>
2025-12-02 10:45:20 +0800

    Enhance deployment and execution scripts
    Refines the deploy.sh script by adding clear output messages during the creation and activation of the virtual environment, as well as during the installation of requirements. The run.sh script is updated to include feedback when activating the virtual environment and running the application, improving user experience and clarity during setup and execution.

--- Ubuntu24.sh
+++ Ubuntu24.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# sudo apt update && sudo apt upgrade -y
-
-# sudo apt install -y python3 python3-pip python3-venv
-
 clear
 
-echo "Creating virtual environment..."
-python3 -m venv venv
+echo "Updating and upgrading packages..."
+sudo apt update && sudo apt upgrade -y
 echo "================================================"
 echo "Done"
 echo "================================================"
 
 clear
 
-echo "Activating virtual environment..."
-source venv/bin/activate
+echo "Installing MariaDB..."
+sudo apt install -y mariadb-server
 echo "================================================"
 echo "Done"
 echo "================================================"
 
 clear
 
-echo "Installing requirements..."
-pip install -r src/requirements.txt
+echo "Installing Python..."
+sudo apt install -y python3 python3-pip python3-venv
 echo "================================================"
 echo "Done"
 echo "================================================"
--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-source venv/bin/activate
+#!/bin/bash
 
+clear
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "================================================"
+echo "Done"
+echo "================================================"
+
+clear
+
+echo "Running application..."
 python3 src/app.py
+echo "================================================"
+echo "Done"
+echo "================================================"
